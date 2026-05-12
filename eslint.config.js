3d03f8c5392fcdb1f2d9763092f96c9ccea46f07
franco.grion <franco.grion@gmail.com>
2026-05-12 11:07:43 +0200

    Refactor shared.js file

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+const js = require("@eslint/js");
+const prettier = require("eslint-config-prettier");
+
+const nodeGlobals = {
+  __dirname: "readonly",
+  Buffer: "readonly",
+  console: "readonly",
+  global: "readonly",
+  module: "readonly",
+  process: "readonly",
+  require: "readonly",
+};
+
+const jestGlobals = {
+  afterAll: "readonly",
+  afterEach: "readonly",
+  beforeAll: "readonly",
+  beforeEach: "readonly",
+  describe: "readonly",
+  expect: "readonly",
+  it: "readonly",
+  jest: "readonly",
+  test: "readonly",
+};
+
+module.exports = [
+  {
+    ignores: ["coverage/**", "dest/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "commonjs",
+      globals: nodeGlobals,
+    },
+  },
+  {
+    files: ["test/**/*.js", "**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: jestGlobals,
+    },
+  },
+  prettier,
+];
